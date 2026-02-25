6f03b76fea790728852f411088ddc4d43eab474b
Fedor Urvanov <fedor@urvanov.ru>
2026-02-25 14:31:53 +0300

    Пример для раздела Spring Boot книги

---
+++ 02.06.16-spring-boot/src/main/java/ru/urvanov/javaexamples/springdockercompose/HomeController.java
@@ -0,0 +1,20 @@
+package ru.urvanov.javaexamples.springdockercompose;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(HomeController.class);
+
+    @GetMapping("/")
+    public String home() {
+        logger.info("Received GET request at /");
+        return "Hello, World!";
+    }
+
+}
---
+++ 02.06.16-spring-boot/src/main/java/ru/urvanov/javaexamples/springdockercompose/SpringDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package ru.urvanov.javaexamples.springdockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerComposeApplication.class, args);
+    }
+
+}
