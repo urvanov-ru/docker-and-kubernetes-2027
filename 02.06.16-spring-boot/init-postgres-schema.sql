c31a83a0482372edd3636929c627684f510cb7ab
Fedor Urvanov <fedor@urvanov.ru>
2026-02-26 15:32:19 +0300

    PostgreSQL в примере интеграции Spring и Docker Compose

---
+++ 02.06.16-spring-boot/init-postgres-schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE table1(
+  column1 varchar(100) NOT NULL
+);
+
+INSERT INTO table1 (column1)
+VALUES ('Hello, World!');
--- 02.06.16-spring-boot/src/main/java/ru/urvanov/javaexamples/springdockercompose/HomeController.java
+++ 02.06.16-spring-boot/src/main/java/ru/urvanov/javaexamples/springdockercompose/HomeController.java
@@ -1,7 +1,15 @@
 package ru.urvanov.javaexamples.springdockercompose;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +19,26 @@ public class HomeController {
     private static final Logger logger = LoggerFactory
             .getLogger(HomeController.class);
 
+    @Autowired
+    private DataSource dataSource;
+
     @GetMapping("/")
     public String home() {
         logger.info("Received GET request at /");
-        return "Hello, World!";
+        try {
+            Connection connection = dataSource.getConnection();
+            try (Statement statement = connection.createStatement();
+                    ResultSet rs = statement.executeQuery("SELECT column1 from table1")) {
+                if (rs.next()) {
+                    return rs.getString(1);
+                } else {
+                    return "No data found";
+                }
+            }
+        } catch (SQLException e) {
+            logger.error("Error executing SQL query", e);
+            return "Database error";
+        }
     }
 
 }
