01539d89e605f975b2f418b7283312fc0cbe0269
Fedor Urvanov <fedor@urvanov.ru>
2026-07-18 19:43:12 +0300

    Комментарии для книги.

--- 03.02.05-pow-golang/pow-golang.go
+++ 03.02.05-pow-golang/pow-golang.go
@@ -1,3 +1,5 @@
+// Простейший http-сервис возведения в степень.
+// https:/urvanov.ru
 package main
 
 import (
@@ -7,28 +9,39 @@ import (
     "strconv"
 )
 
+// Простейший обработчик запросов.
+// Считывает входные параметры из URL и 
+// пишет результат в ответ сервера.
 func pow(w http.ResponseWriter, r *http.Request) {
     
     values := r.URL.Query()
-        
+    
+    // Считываем основание в float64.
     base, err := strconv.ParseFloat(values.Get("base"), 64);
     if (err != nil) {
-        http.Error(w, "Incorrect base parameter.", http.StatusBadRequest);
+        http.Error(w, "Incorrect base parameter.",
+                http.StatusBadRequest);
         return;
     }
-        
-    exponent, err := strconv.ParseFloat(values.Get("exponent"), 64);
+    
+    // Считываем степень в float64.
+    exponent, err := strconv.ParseFloat(
+            values.Get("exponent"), 64);
     if (err != nil) {
-        http.Error(w, "Incorrect exponent parameter.", http.StatusBadRequest);
+        http.Error(w, "Incorrect exponent parameter.",
+                http.StatusBadRequest);
         return;
     }
 
+    // Пишем в качестве ответа сервера результат возведения
+    // в степень.
     fmt.Fprintf(w, "Result: %f\n", math.Pow(base, exponent));
 }
 
-
+// Стартовая точка программы.
 func main() {
-    fmt.Println("Usage example: http://localhost:8087?base=2&exponent=3")
+    fmt.Println("Usage example:")
+    fmt.Println("http://localhost:8087?base=2&exponent=3")
     http.HandleFunc("/", pow)
     http.ListenAndServe(":8087", nil)
 }
